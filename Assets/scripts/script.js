51c5f6bba0a7bec6c70f78831d669e6e117cb34c
Teylor Smith <teylorsmith34@gmail.com>
2023-05-04 23:32:38 -0400

    restructured html

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,4 +1,5 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
   // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
